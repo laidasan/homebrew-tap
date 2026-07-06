e6ba333f288621f07e132b99c86e5e0ab9415e17
Samura.chen <tp6m4xup6tp6@gmail.com>
2026-07-06 13:30:31 +0800

    feat: update dev local manager rb - v0.0.0-beta.

--- Casks/dev-local-manager.rb
+++ Casks/dev-local-manager.rb
@@ -1,11 +1,11 @@
 cask "dev-local-manager" do
   version "0.0.0-beta"
-  sha256 "TODO"
+  sha256 "533ffdfbb1462552f9f469a9f827abc41ef6dd9dcf01a335d3f93bc87f063750"
 
-  url "https://github.com/YOUR_USERNAME/dev-local-manager/releases/download/v#{version}/DevLocalManager.dmg"
+  url "https://github.com/laidasan/dev-local-manager/releases/download/v#{version}/DevLocalManager.dmg"
   name "DevLocalManager"
   desc "Local dev environment launcher for multi-repo projects"
-  homepage "https://github.com/YOUR_USERNAME/dev-local-manager"
+  homepage "https://github.com/laidasan/dev-local-manager"
 
   app "DevLocalManager.app"
 end
